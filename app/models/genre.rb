5f4af8b3d44fc064358b7660c4329e44fb760ea4
Annie Bourke <aabourke@gmail.com>
2018-05-11 22:06:50 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.names
+    self.artists.select_all("SELECT name")
   end
 end
